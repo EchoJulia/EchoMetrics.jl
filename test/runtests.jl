30fc1fd3a910529c114620cc0c1d268ecdbf4ebd
Rob Blackwell <rob@robblackwell.com>
2021-10-05 13:16:11 +0100

    Fix test

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,13 +5,10 @@ using EchoMetrics
 Sv = [ -70, -60, - 50]
 H = 30
 
-density = mean_volume_backscattering_strength(Sv, H)
+density = mean_volume_backscattering_strength(Sv)
 
-@test trunc(Int, density) == -64
+@test trunc(Int, density) == -54
 
-abundance = area_backscattering_strength(Sv)
+abundance = area_backscattering_strength(Sv, H)
 
-@test trunc(Int, abundance) == -49
-
-
-
+@test trunc(Int, abundance) == -39
